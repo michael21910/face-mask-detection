07a5c9032c3aef3432b5bbdea0cb71f84d087bf2
Keywind <watersprayer127@gmail.com>
2021-11-12 12:23:40 +0800

    Renamed masked_faces to withMask for consistency.

--- withMask/rename new images.py
+++ withMask/rename new images.py
@@ -2,10 +2,15 @@ import os, pandas
 
 supported_images = [ ".jpg", ".png" ]
 
+name_format = "withMask"
+
 num_correct = 0 
 
+_filename = "filename.csv"
+
 def correct_name(filename):
-    return ("masked_faces_no" in filename)
+    global name_format
+    return (name_format in filename)
 
 def should_rename(filename):
     global supported_images, num_correct
@@ -21,9 +26,12 @@ filelist = [ name for name in os.listdir("./") if should_rename(name) ]
 dictionary = { "newname" : [], "oldname" : [] }
 
 for index, name in enumerate(filelist):
-    oldname, newname = name, "masked_faces_no" + str(num_correct + 1 + index) + name[-4:]
+    oldname, newname = name, name_format + str(num_correct + 1 + index) + name[-4:]
     dictionary["newname"].append(newname)
     dictionary["oldname"].append(oldname)
     os.rename(oldname, newname)
-    
-pandas.DataFrame(dictionary).to_csv("filename.csv", mode = "a", header = False, index = False)
+
+if (os.path.isfile(_filename)):
+    pandas.DataFrame(dictionary).to_csv(_filename, mode = "a", header = False, index = False)
+else:
+    pandas.DataFrame(dictionary).to_csv(_filename, header = True, index = False)
